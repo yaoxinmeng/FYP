a72615fc9bfe918ca6d12d3987a86ea440650396
yaoxinmeng <yaoxinmeng@gmail.com>
2019-10-26 17:16:41 +0800

    update

--- labelling/augmentation.py
+++ labelling/augmentation.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 # paths
 data_path = 'labelled_data/data'
@@ -7,7 +8,8 @@ label_path = 'labelled_data/label'
 
 # global variables
 count = len(os.listdir(data_path))
-
+data_list = []
+label_list = []
 for subdir, dirs, files in os.walk(data_path):
     for file in files:
         # read data and labels
@@ -50,5 +52,19 @@ for subdir, dirs, files in os.walk(data_path):
         cv2.imwrite(out_label_path, flipped_label)
         count += 1
 
+        # Add to list
+        data_list.append(data)
+        data_list.append(flipped_data)
+        data_list.append(bright_data)
+        data_list.append(flipped_bright_data)
+        label_list.append(label)
+        label_list.append(flipped_label)
+        label_list.append(label)
+        label_list.append(flipped_label)
+
         if count % 1000 == 0:
             print('Finished', count, 'images')
+
+data_list = np.array(data_list)
+label_list = np.array(label_list)
+np.savez('outfile', data=data_list, label=label_list)
--- labelling/train_model.py
+++ labelling/train_model.py
@@ -13,6 +13,8 @@ from matplotlib import pyplot as plt
 data_path = 'labelled_data/data'
 label_path = 'labelled_data/label'
 SPLIT = 0.7
+BATCH_SIZE = 10
+SHUFFLE_BUFFER_SIZE = 10
 
 
 # FUNCTIONS
@@ -24,10 +26,10 @@ def pair_data_label(data_path, label_path):
         for file in files:
             # get image from path given
             filename = os.path.join(data_path, file)
-            data = cv2.imread(filename) / 255.0
+            data = cv2.imread(filename)
             # get label with identical name from path
             filename = os.path.join(label_path, file)
-            label = cv2.imread(filename) / 255.0
+            label = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
             # add arrays to list
             x.append(data)
@@ -39,6 +41,19 @@ def pair_data_label(data_path, label_path):
     train_y = y[0 : split_point]
     test_x = x[split_point+1 : len(x)]
     test_y = y[split_point+1 : len(y)]
+
+    # convert into array and reshape
+    train_size = len(train_x)
+    test_size = len(test_x)
+    train_x = np.array(train_x)
+    train_x.reshape(train_size, 256, 256, 3).astype('float32')
+    train_y = np.array(train_y)
+    train_y.reshape(train_size, 256, 256, 1).astype('float32')
+    test_x = np.array(test_x)
+    test_x.reshape(test_size, 256, 256, 3).astype('float32')
+    test_y = np.array(test_y)
+    test_y.reshape(test_size, 256, 256, 1).astype('float32')
+
     return (train_x, train_y), (test_x, test_y)
 
 
@@ -46,7 +61,7 @@ def generate_model():
   model = tf.keras.models.Sequential()
 
   # takes in a RGB input of 256x256, output filters = 64
-  model.add(layers.Conv2D(64, (5,5), strides=(2,2), padding='same', input_shape=[256,256,3]))
+  model.add(layers.Conv2D(64, (5,5), strides=(2,2), padding='same', input_shape=(256,256,3)))
   assert model.output_shape == (None, 128, 128, 64)   # check output shape
   model.add(layers.BatchNormalization())
   model.add(layers.LeakyReLU())
@@ -85,26 +100,34 @@ def generate_model():
 
 # MAIN FUNCTION
 def main():
-    # Get data
+    # Retrieve and normalize data
     (train_images, train_labels), (test_images, test_labels) = pair_data_label(data_path, label_path)
-    print(train_images[0].shape)
+    train_images, train_labels, test_images, test_labels = train_images/255.0, train_labels/255.0, test_images/255.0, test_labels/255.0
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+    train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+
     # Create neural network and train it
     model = generate_model()
+    model.summary()
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    history = model.fit(train_images, train_labels, validation_data=(test_images,test_labels), epochs=20)  # validate using test data
-    # Get stats from history object
-    history_dict = history.history
-    print(history_dict.keys())    # get keys for history object
-    acc = history.history['accuracy']            # Train data accuracy
-    val_acc = history.history['val_accuracy']    # Test data accuracy
-    epochs = range(len(acc))
-    # Plot accuracy vs epochs
-    plt.title('Training and validation accuracy')
-    plt.plot(epochs, acc, color='blue', label='Train')
-    plt.plot(epochs, val_acc, color='red', label='Test')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend()
+    history = model.fit(train_dataset, validation_data=test_dataset, epochs=20)  # validate using test data
+    #
+    # # Get stats from history object
+    # history_dict = history.history
+    # print(history_dict.keys())    # get keys for history object
+    # acc = history.history['accuracy']            # Train data accuracy
+    # val_acc = history.history['val_accuracy']    # Test data accuracy
+    # epochs = range(len(acc))
+    #
+    # # Plot accuracy vs epochs
+    # plt.title('Training and validation accuracy')
+    # plt.plot(epochs, acc, color='blue', label='Train')
+    # plt.plot(epochs, val_acc, color='red', label='Test')
+    # plt.xlabel('Epoch')
+    # plt.ylabel('Accuracy')
+    # plt.legend()
 
 
 if __name__ == '__main__' :
