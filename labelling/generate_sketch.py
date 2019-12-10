3f826eea1d225783a2320b208571238307093098
yaoxinmeng <yaoxinmeng@gmail.com>
2019-12-10 09:40:47 +0800

    label

--- labelling/generate_sketch.py
+++ labelling/generate_sketch.py
@@ -4,7 +4,7 @@ import numpy as np
 import argparse
 
 # threshold value for pixel to qualify as a label
-threshold = 0.8
+threshold = 0.1
 # Hough Transform parameters
 minLineLength = 20
 maxLineGap = 5
@@ -17,31 +17,30 @@ def read_file(file):
     # Comment out this section as necessary
     ################################################
     label = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    label = np.array(label)
-    #label.reshape(256, 256, 1).astype('float32')
+    cv2.imshow('label', label)
+    label = np.array(label).astype('float32')
     label =  label/ 255.0
     ################################################
-    for points in label:
-        for point in points:
+
+    # categorise labels into either black (0) or white (1), and
+    # inverse white and black for Hough Transform later
+    with np.nditer(label, op_flags=['readwrite']) as temp:
+        for point in temp:
             if point <= threshold:
-                point = 0
+                point[...] = 1
             else:
-                point = 1
-    cv2.imshow('label', label)
-    cv2.waitKey(0)
-    return label * 255.0
+                point[...] = 0
+    return np.uint8(label * 255.0)
 
 
-def houghtransform(file):
-    label = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+def houghtransform(label):
     img = np.copy(label)*0 + 255
-    edges = cv2.Canny(label, 50, 150, apertureSize = 3, L2gradient=True)
-    lines = cv2.HoughLinesP(edges, rho, theta, houghthreshold, minLineLength, maxLineGap)
+    lines = cv2.HoughLinesP(label, rho, theta, houghthreshold, minLineLength, maxLineGap)
     if lines.any():
         for line in lines:
             for x1,y1,x2,y2 in line:
                 cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-    cv2.imshow('canny', edges)
+    cv2.imshow('edges', label)
     cv2.imshow('hough', img)
     cv2.waitKey(0)
     cv2.imwrite('houghlines5.jpg',img)
@@ -51,5 +50,5 @@ parser = argparse.ArgumentParser()
 parser.add_argument('filename', type=str, help='Name of input label file')
 args = parser.parse_args()
 
-#label = read_file(args.filename)
-houghtransform(args.filename)
+label = read_file(args.filename)
+houghtransform(label)
